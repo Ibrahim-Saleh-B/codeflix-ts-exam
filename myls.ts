93b30656ee61f17c411b0508bfdcd123259c3fa8
isaleh <isaleh@generation-ia.com>
2021-01-13 17:13:55 +0100

    ls -l

--- myls.ts
+++ myls.ts
@@ -21,3 +21,30 @@
 // }
 
 // myls()
+
+//3 ls-l Je n'ai pas réussi à accéder au dossier et à le faire sur tout ce qu'il contient, aussi à converitr les uid et gid en string malgré le require('userid')...
+var fs = require('fs');
+var Mode = require('stat-mode');
+// var userid = require("userid");
+// const exec = require('child_process').exec;
+
+function lsL() {
+
+
+        fs.stat(process.argv[2], function (err:string, stat:string) {
+            if (err) throw err;
+                
+            var mode = new Mode(stat);
+            var nlink = fs.statSync(process.argv[2]).nlink;
+            var uid = fs.statSync(process.argv[2]).uid; 
+            var gid = fs.statSync(process.argv[2]).gid;
+            var size = fs.statSync(process.argv[2]).size;
+            var file = process.argv[2];
+            var date = fs.statSync(process.argv[2]).birthtime;
+
+                console.log(mode.toString(), nlink, uid, gid, size,date, file);
+
+            });
+}
+
+lsL();
